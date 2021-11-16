599521e4c559e8748535041a8d9a73893ed93497
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-11-15 21:40:30 -0500

    Agregar tareas otra forma

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas
 
-function App() {
-
-  const [tarea, guardarTarea] = useState({
-    nombre: ''
-  })
-
-  const {nombre} = tarea;
-
-  const [task, getTask] = useState([]);
-
-
-    const handleChange = e => {
-      guardarTarea({
-          [e.target.name] : e.target.value
-      })
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "",
+    list:[]};
   }
 
-    const agregarTarea = (e) =>{
-      e.preventDefault();
+ 
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
 
-      getTask([...task, tarea.nombre]);
+  sendName(event) {
+    event.preventDefault();
+    this.setState({
+      list: this.state.list.concat(this.state.name)
+    });
+    this.state.name = ""
+  }
+  
 
-      guardarTarea({
-        nombre: ''
-      });
-    }
-
+  render() {
     return (
       <div className="wrapper">
         <div className="list">
@@ -37,19 +34,18 @@ function App() {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
-            { task.map((li)=><li>{li}</li>)}
+              {this.state.list.map((value)=>
+                <li key={value}>{value}</li>
+              )}
           </ul>
-           <form
-            onSubmit={agregarTarea}
-           >
-             <input type="text" id="new-task" name="nombre" value={nombre} placeholder="Ingresa una tarea y oprime Enter"
-              onChange={handleChange}
-             />
+           <form onSubmit={this.sendName.bind(this)}>
+             <input type="text" id="new-task" value={this.state.name} placeholder="Ingresa una tarea y oprime Enter" onChange={this.updateName.bind(this)}/>
            </form>
         </div>
       </div>
     )
-  
+  }
+
 }
 
 
